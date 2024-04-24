608c2fccb4d6ca3b2522bfab037cc7a47697d26f
hae.manasses <hae.manasses.neto@hyundai-brasil.com>
2024-04-24 06:02:37 -0300

    Final CRUD - Category

--- LojaWeb/Controllers/CategoryController.cs
+++ LojaWeb/Controllers/CategoryController.cs
@@ -7,11 +7,11 @@ namespace LojaWeb.Controllers
 	public class CategoryController : Controller
 	{
 		private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+		public CategoryController(ApplicationDbContext db)
 		{
 			_db = db;
 		}
-        public IActionResult Index()
+		public IActionResult Index()
 		{
 			List<Category> objCategoryList = _db.Categories.ToList();
 			return View(objCategoryList);
@@ -20,6 +20,89 @@ namespace LojaWeb.Controllers
 		public IActionResult Create()
 		{
 			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The Display Order cannot exactle match the Name.");
+			}
+
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category created Successfully";
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+		public IActionResult Edit(int? categoryId)
+		{
+			if (categoryId == null || categoryId == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(categoryId);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.CategoryId==categoryId);
+			//Category? categoryFromDb2 = _db.Categories.Where(u=>u.CategoryId==categoryId).FirstOrDefault();
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated Successfully";
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				return View();
+			}
+		}
+
+		public IActionResult Delete(int? categoryId)
+		{
+			if (categoryId == null || categoryId == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(categoryId);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.CategoryId==categoryId);
+			//Category? categoryFromDb2 = _db.Categories.Where(u=>u.CategoryId==categoryId).FirstOrDefault();
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? categoryId)
+		{
+			Category obj = _db.Categories.Find(categoryId);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted Successfully";
+			return RedirectToAction("Index");
 		}
 	}
 }
